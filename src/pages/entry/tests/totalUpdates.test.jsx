22aac5eb2e5ad25dd96353f881316a0d4b9479f5
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2021-12-31 10:11:16 +0300

    code and test topping updating subtotal

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -24,3 +24,24 @@ test("update scoop subtotal when the scoops change", async () => {
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotal when toppings change", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsTotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  const hotfudgeCheckbox = screen.getByRole("checkbox", { name: "Hot fudge" });
+  userEvent.click(hotfudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("3.00");
+
+  userEvent.click(hotfudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+});
